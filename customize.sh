c77bc003fd4b1864733b763fbc5187a2187b7c81
yunshaoyy <2015219629@qq.com>
2025-05-26 23:46:10 +0800

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,10 @@
+SKIPUNZIP=0
+ASH_STANDALONE=0
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "Prop fake miui"
+  ui_print "*******************************"
+}
+
+set_perm_recursive  $MODPATH  0  0  0755  0644
